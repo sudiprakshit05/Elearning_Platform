6fe503d064f54d73cebd36d917096045729043a5
SweetyPaul19 <sweetypaul2005@gmail.com>
2026-04-11 13:46:35 +0530

    update admin.py

--- Elearningapp/admin.py
+++ Elearningapp/admin.py
@@ -1,7 +1,5 @@
 from django.contrib import admin
 from Elearningapp.models import *
 admin.site.register(Elearningadmin)
-# Register your models here.
-
 admin.site.register(coursetype)
 # Register your models here.
